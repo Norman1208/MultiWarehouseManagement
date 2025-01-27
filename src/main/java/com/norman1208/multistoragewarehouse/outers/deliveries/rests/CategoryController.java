b4620b1cea8ae66801f74ecc13f18540ff28b838
norman1208 <achmadnorman1208@gmail.com>
2025-01-27 12:22:25 +0800

    CorsConfig feature to handle connection to frontend

--- src/main/java/com/norman1208/multistoragewarehouse/outers/deliveries/rests/CategoryController.java
+++ src/main/java/com/norman1208/multistoragewarehouse/outers/deliveries/rests/CategoryController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/product-categories")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CategoryController {
 
     @Autowired
